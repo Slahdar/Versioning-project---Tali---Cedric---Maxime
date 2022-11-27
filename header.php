87b08ecda240f80ea08f9d9c93f5e63c8a9b6004
cedric dousset <cedricdsst@gmail.com>
2022-11-27 03:02:57 +0100

    FIXE :
    Correction chemins d'acces href

--- header.php
+++ header.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
     <meta name="description" content="Litho is a clean and modern design, BootStrap 5 responsive, business and portfolio multipurpose HTML5 template with 37+ ready homepage demos.">
     <!-- favicon icon -->
-    <link rel="shortcut icon" href="/images/images/logo/favicon.png">
-    <link rel="apple-touch-icon" href="/images/images/logo/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/images/images/logo/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/images/images/logo/apple-touch-icon-114x114.png">
+    <link rel="shortcut icon" href="images/images/logo/favicon.png">
+    <link rel="apple-touch-icon" href="images/images/logo/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/images/logo/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/images/logo/apple-touch-icon-114x114.png">
     <!-- style sheets and font icons  -->
-    <link rel="stylesheet" type="text/css" href="/css/font-icons.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/theme-vendors.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/style.css" />
-    <link rel="stylesheet" type="text/cdss" href="/css/responsive.css" />
+    <link rel="stylesheet" type="text/css" href="css/font-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="css/theme-vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/cdss" href="css/responsive.css" />
     <!-- revolution slider -->
-    <link rel="stylesheet" type="text/css" href="/revolution/css/settings.css">
-    <link rel="stylesheet" type="text/css" href="/revolution/css/layers.css">
-    <link rel="stylesheet" type="text/css" href="/revolution/css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
 </head>
 <body data-mobile-nav-style="classic">
 <!-- start header -->
@@ -27,9 +27,9 @@
         <div class="container-fluid nav-header-container">
             <div class="col-auto col-sm-6 col-lg-2 me-auto ps-lg-0">
                 <a class="navbar-brand" href="page1.php">
-                    <img src="/images/images/logo/logo-white.png" data-at2x="/images/images/logo/logo-white.png" class="default-logo" alt="">
-                    <img src="/images/images/logo/logo-white.png" data-at2x="/images/images/logo/logo-white.png" class="alt-logo" alt="">
-                    <img src="/images/images/logo/logo-white.png" data-at2x="/images/images/logo/logo-white.png" class="mobile-logo" alt="">
+                    <img src="/images/images/logo/logo-white.png" data-at2x="images/images/logo/logo-white.png" class="default-logo" alt="">
+                    <img src="/images/images/logo/logo-white.png" data-at2x="images/images/logo/logo-white.png" class="alt-logo" alt="">
+                    <img src="/images/images/logo/logo-white.png" data-at2x="images/images/logo/logo-white.png" class="mobile-logo" alt="">
                 </a>
             </div>
             <div class="col-auto menu-order px-lg-0">
@@ -42,15 +42,15 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav alt-font">
                         <li class="nav-item dropdown megamenu">
-                            <a href="/page1.php" class="nav-link">Page 1</a>
+                            <a href="page1.php" class="nav-link">Page 1</a>
                             <i class="fa fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
                         </li>
                         <li class="nav-item dropdown simple-dropdown">
-                            <a href="/page2.php" class="nav-link">Page 2</a>
+                            <a href="page2.php" class="nav-link">Page 2</a>
                             <i class="fa fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
                         </li>
                         <li class="nav-item dropdown simple-dropdown">
-                            <a href="/page3.php" class="nav-link">page 3</a>
+                            <a href="page3.php" class="nav-link">page 3</a>
                             <i class="fa fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
                         </li>
 
